370d604ad1161bbe3b8594f434a0f16689b79f3f
Sara Khaled Ali <119273156+sara-kaz@users.noreply.github.com>
2024-07-26 05:23:28 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,19 +13,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.SelectionMode;
 
 public class Main extends Application {
-@Override
-public void start(Stage stage) throws Exception {
-	Parent root = FXMLLoader.load(getClass().getResource("Lost&Found.fxml"));
-	stage.setTitle("AUBH Lost and Found");
-	Scene scene = new Scene(root);
-	stage.setMinWidth(930);
-	stage.setMinHeight(710);
-	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-	stage.setScene(scene);
-	stage.show();
-}
-
-public static void main(String[] args) {
-launch(args);
-}
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Lost&Found.fxml"));
+		stage.setTitle("AUBH Lost and Found");
+		Scene scene = new Scene(root);
+		stage.setMinWidth(930);
+		stage.setMinHeight(710);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+        }
+	
+	public static void main(String[] args) {
+		launch(args);
+        }
 }
